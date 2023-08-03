1a2a70a2345d86664a7cc31dea52ae716d905be2
IG <iikim2511@gmail.com>
2023-08-03 12:38:19 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// TabPage.js
+import React, { useState } from 'react';
+import "./TabPage.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <TabPage/>
     </div>
   );
 }
+
+
+const TabPage = () => {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+  const handleTabClick = (index) => {
+    setActiveTabIndex(index);
+  };
+
+  return (
+    <div className="tab-page">
+      <div className="tabs">
+    
+        <Tab label="Tab 1" index={0} activeTabIndex={activeTabIndex} onClick={handleTabClick} />
+        <Tab label="Tab 2" index={1} activeTabIndex={activeTabIndex} onClick={handleTabClick} />
+        <Tab label="Tab 3" index={2} activeTabIndex={activeTabIndex} onClick={handleTabClick} />
+      </div>
+      <div className="tab-content">
+        {activeTabIndex === 0 && <p>Content of Tab 1</p>}
+        {activeTabIndex === 1 && <p>Content of Tab 2</p>}
+        {activeTabIndex === 2 && <p>Content of Tab 3</p>}
+      </div>
+    </div>
+  );
+};
+
+// props
+const Tab = ({ label, index, activeTabIndex, onClick }) => {
+    const isActive = index === activeTabIndex;
+  
+    return (
+      <div
+        className={`tab ${isActive ? 'active' : ''}`}
+        onClick={() => onClick(index)}
+      >
+        {label}
+      </div>
+    );
+  };
+
 
 export default App;
